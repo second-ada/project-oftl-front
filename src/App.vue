35ad8859bbcdc08b12558f854a1dd729b6e375c6
Don Raulzito <don-raulzito@proton.me>
2023-08-26 15:41:41 -0300

    project

---
+++ src/App.vue
@@ -0,0 +1,5 @@
+<template>
+  <main class="w-screen h-screen">
+    <RouterView />
+  </main>
+</template>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,10 @@
+<template>
+  <section class="
+    w-full h-full
+    flex flex-col items-center justify-center
+    gap-52
+  ">
+    <h1>Olá, Mundo!</h1>
+    <router-link to="/players">Começar</router-link>
+  </section>
+</template>
